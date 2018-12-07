b5c7b19a1cf4f11eecd292bd9387ba237c896916
fieldpalmer <gfieldpalmer@gmail.com>
2018-12-07 16:38:55 -0500

    working app

---
+++ models/Article.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  // summary: {
+  //   type: String,
+  //   required: true
+  // },
+  link: {
+    type: String,
+    required: true
+  },
+  img: {
+    type: String,
+    // required: true
+  },
+  author: {
+    type: String,
+    // required: true
+  },
+  date: {
+    type: String,
+    // default: Date.now
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
