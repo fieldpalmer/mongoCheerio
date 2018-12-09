d628ba2253bb1763b2bd918ab060337d4d36f188
fieldpalmer <gfieldpalmer@gmail.com>
2018-12-08 19:12:36 -0500

    added processenv to port var

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3031;
+var PORT = process.env.MONGODB_URI || 3031;
 
 // Initialize Express
 var app = express();
